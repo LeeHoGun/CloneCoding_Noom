a6b449283dc25fbbbd68a01553a007da6b904730
LeeHoGun <ss526611@gmail.com>
2022-06-14 18:02:50 +0900

    noom_ws_inst

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 // console.log("hello");
-import express from 'express';
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
 
 const app = express();
 
@@ -10,4 +12,9 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000,handleListen);
+// app.listen(3000,handleListen);
+const server = http.createServer(app);
+// Node.js에 기본으로 내장된 HTTP 패키지를 사용해서 express로 만든 서버 애플리케시연 제공
+const wss = new WebSocket.Server({ server });
+// 웹 소켓 서버를 생성하면서 여기에 HTTP서버를 전달
+server.listen(3000, handleListen);
