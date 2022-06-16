973135290515b8fa7dd5f8682d209ff35a841e4d
LeeHoGun <ss526611@gmail.com>
2022-06-16 18:07:02 +0900

    noom_websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,3 +2,20 @@
 // const socket = new WebSocket("http://localhost:3000");
 const socket = new WebSocket(`ws://${window.location.host}`);
 // `` <-- 1왼쪽에 있는 키
+
+socket.addEventListener("open", () => {
+    console.log("Connect to Server")
+}); // "open" 이벤트 -> 서버에 연결
+
+socket.addEventListener("message", (message) => {
+    console.log("Just got this:", message.data, "from the server")
+    // message 객체의 data속성 -> 전달딘 메시지 반환
+}); // "message" 이벤트 -> 메시지 전달 : 메시지 전달 받아서 콘솔에 출력
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server")
+}); // "close" 이벤트 -> 서버 오프라인 상태
+
+setTimeout(() => {
+    socket.send("hello from Browser!");
+}, 5000); //setTimeout 메소드 -> 5000밀리초(5초) 후에 사용자에서 서버에게 메시지 전송
--- src/server.js
+++ src/server.js
@@ -11,17 +11,27 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
+const handleListen = () => console.log("Listening on http://localhost:8081");
 // app.listen(3000,handleListen);
 const server = http.createServer(app);
 // Node.js에 기본으로 내장된 HTTP 패키지를 사용해서 express로 만든 서버 애플리케시연 제공
 const wss = new WebSocket.Server({ server });
 // 웹 소켓 서버를 생성하면서 여기에 HTTP서버를 전달
 
-function handleConnection(socket){
-    console.log(socket)
-}
+/*function handleConnection(socket){
+    // console.log(socket);
+};
 
 wss.on("connection", handleConnection);
+*/
 
-server.listen(3000, handleListen);
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message", (message) => {
+        console.log(`${message}`);
+    });   
+    socket.send("hello!");
+});
+
+server.listen(8081, handleListen);
